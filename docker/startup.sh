54d8454a816111b91098a85ee2e767d77dbe9144
KE Gustafsson <ke.gustafsson@gmail.com>
2026-01-03 17:52:00 +0200

    updates

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,3 +4,18 @@ service dbus restart
 /usr/sbin/avahi-daemon --no-drop-root &
 service bluetooth restart
 /usr/bin/pm2-docker '/usr/lib/node_modules/signalk-server/bin/signalk-server --securityenabled'
+
+
+#!/usr/bin/env sh
+if [ -S /run/dbus/system_bus_socket ] && dbus-send --system --dest=org.freedesktop.DBus --print-reply /org/freedesktop/DBus org.freedesktop.DBus.ListNames >/dev/null 2>&1; then
+    echo "Using host D-Bus (socket mounted from host)"
+else
+    echo "Starting container D-Bus and Avahi services"
+    service dbus restart
+    /usr/sbin/avahi-daemon -k 2>/dev/null
+    /usr/sbin/avahi-daemon --no-drop-root &
+    # service bluetooth restart
+fi
+#/usr/bin/pm2-docker 'node --trace-deprecation /usr/lib/node_modules/signalk-server/bin/signalk-server --securityenabled'
+/usr/bin/pm2-docker 'node --trace-deprecation /home/node/signalk/node_modules/signalk-server/bin/signalk-server --securityenabled'
+#/usr/bin/pm2-docker 'node --trace-deprecation /home/node/signalk/node_modules/.bin/signalk-server --securityenabled'
